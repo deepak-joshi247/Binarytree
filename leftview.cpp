bec41d16054f5022901c65bf6ea7d002370544da
hehehaha247 <deepaksjoshi247@gmail.com>
2025-08-19 13:19:15 +0530

    Create leftview.cpp
    Learn vertical but that's not all !

---
+++ leftview.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    vector<int> leftView(Node *root) {
+        map<int,pair<bool,int>> m;
+        queue<pair<Node*,int>> q;
+        vector<int> ans;
+        if(root == NULL){
+            return ans;
+        }
+        q.push(make_pair(root,0));
+        while(!q.empty()){
+            pair<Node*,int> temp = q.front();
+            q.pop();
+            Node* frontnode = temp.first;
+            int level = temp.second;
+            if(m[level].first != true){
+                m[level].second = frontnode->data;
+                m[level].first = true;
+            }
+            if(frontnode->left != NULL){
+                q.push(make_pair(frontnode->left,level+1));
+            }
+            if(frontnode->right != NULL){
+                q.push(make_pair(frontnode->right,level+1));
+            }
+        }
+        for(auto i: m){
+            ans.push_back(i.second.second);
+        }
+        return ans;
+    }
+};
